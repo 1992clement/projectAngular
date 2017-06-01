5a31c66037a9a6545aaec49321193adbbcd0bfe2
BPurified <a.lorraine.vervisch@gmail.com>
2017-06-01 16:53:21 +0200

    Fix mistake

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ import { AppComponent } from './app.component';
 import { FavorisComponent } from './favoris/favoris.component';
 import { SearchArtistComponent } from './search-artist/search-artist.component';
 import { SearchArtistEventsComponent } from './search-artist-events/search-artist-events.component';
-import { SearchEventWeatherComponent } from './search-event-weather/search-event-weather.component';
+import { WeatherComponent } from './weather/weather.component';
 
 import { HomeComponent } from './home/home.component';
 import { CallbackComponent } from './callback/callback.component';
@@ -30,7 +30,7 @@ const routes: Routes = [
   },
   { 
     path: 'artist/:name/:city/:event/weather',
-    component: SearchEventWeatherComponent
+    component: WeatherComponent
   },
   { 
     path: 'favoris/', 
